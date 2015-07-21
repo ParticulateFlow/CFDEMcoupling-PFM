c41e2966d5f5a2109443024ca0fdb36ea3ab6649
Thomas Lichtenegger <thomas.lichtenegger@jku.at>
2015-07-21 14:41:54 +0200

    Added Beetstra drag law (monodisperse case)

---
+++ src/lagrangian/cfdemParticle/subModels/forceModel/BeetstraDrag/BeetstraDrag.C
@@ -0,0 +1,240 @@
+/*---------------------------------------------------------------------------*\
+    CFDEMcoupling - Open Source CFD-DEM coupling
+
+    CFDEMcoupling is part of the CFDEMproject
+    www.cfdem.com
+-------------------------------------------------------------------------------
+License
+    This file is part of CFDEMcoupling.
+
+    CFDEMcoupling is free software; you can redistribute it and/or modify it
+    under the terms of the GNU General Public License as published by the
+    Free Software Foundation; either version 3 of the License, or (at your
+    option) any later version.
+
+    CFDEMcoupling is distributed in the hope that it will be useful, but WITHOUT
+    ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+    FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+    for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with CFDEMcoupling; if not, write to the Free Software Foundation,
+    Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA
+
+Description
+    This code is designed to realize coupled CFD-DEM simulations using LIGGGHTS
+    and OpenFOAM(R). Note: this code is not part of OpenFOAM(R) (see DISCLAIMER).
+\*---------------------------------------------------------------------------*/
+
+#include "error.H"
+
+#include "BeetstraDrag.H"
+#include "addToRunTimeSelectionTable.H"
+#include "averagingModel.H"
+
+// * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
+
+namespace Foam
+{
+
+// * * * * * * * * * * * * * * Static Data Members * * * * * * * * * * * * * //
+
+defineTypeNameAndDebug(BeetstraDrag, 0);
+
+addToRunTimeSelectionTable
+(
+    forceModel,
+    BeetstraDrag,
+    dictionary
+);
+
+
+// * * * * * * * * * * * * * * * * Constructors  * * * * * * * * * * * * * * //
+
+// Construct from components
+BeetstraDrag::BeetstraDrag
+(
+    const dictionary& dict,
+    cfdemCloud& sm
+)
+:
+    forceModel(dict,sm),
+    propsDict_(dict.subDict(typeName + "Props")),
+    velFieldName_(propsDict_.lookup("velFieldName")),
+    U_(sm.mesh().lookupObject<volVectorField> (velFieldName_)),
+    voidfractionFieldName_(propsDict_.lookup("voidfractionFieldName")),
+    voidfraction_(sm.mesh().lookupObject<volScalarField> (voidfractionFieldName_)),
+    UsFieldName_(propsDict_.lookup("granVelFieldName")),
+    UsField_(sm.mesh().lookupObject<volVectorField> (UsFieldName_)),
+    scaleDia_(1.),
+    scaleDrag_(1.)
+{
+    //Append the field names to be probed
+    particleCloud_.probeM().initialize(typeName, "BeetstraDrag.logDat");
+    particleCloud_.probeM().vectorFields_.append("dragForce"); //first entry must  be the force
+    particleCloud_.probeM().vectorFields_.append("Urel");
+    particleCloud_.probeM().scalarFields_.append("Rep");
+    particleCloud_.probeM().scalarFields_.append("betaP");
+    particleCloud_.probeM().scalarFields_.append("voidfraction");
+    particleCloud_.probeM().writeHeader();
+
+    // init force sub model
+    setForceSubModels(propsDict_);
+    // define switches which can be read from dict
+    forceSubM(0).setSwitchesList(0,true); // activate treatExplicit switch
+    forceSubM(0).setSwitchesList(2,true); // activate implDEM switch
+    forceSubM(0).setSwitchesList(3,true); // activate search for verbose switch
+    forceSubM(0).setSwitchesList(4,true); // activate search for interpolate switch
+    forceSubM(0).setSwitchesList(8,true); // activate scalarViscosity switch
+    forceSubM(0).readSwitches();
+
+    particleCloud_.checkCG(true);
+    if (propsDict_.found("scale"))
+        scaleDia_=scalar(readScalar(propsDict_.lookup("scale")));
+    if (propsDict_.found("scaleDrag"))
+        scaleDrag_=scalar(readScalar(propsDict_.lookup("scaleDrag")));
+
+}
+
+
+// * * * * * * * * * * * * * * * * Destructor  * * * * * * * * * * * * * * * //
+
+BeetstraDrag::~BeetstraDrag()
+{}
+
+
+// * * * * * * * * * * * * * * * Member Functions  * * * * * * * * * * * * * //
+
+void BeetstraDrag::setForce() const
+{
+    if (scaleDia_ > 1)
+        Info << "Beetstra using scale = " << scaleDia_ << endl;
+    else if (particleCloud_.cg() > 1){
+        scaleDia_=particleCloud_.cg();
+        Info << "Beetstra using scale from liggghts cg = " << scaleDia_ << endl;
+    }
+
+    const volScalarField& nufField = forceSubM(0).nuField();
+    const volScalarField& rhoField = forceSubM(0).rhoField();
+
+    vector position(0,0,0);
+    scalar voidfraction(1);
+    vector Ufluid(0,0,0);
+    vector drag(0,0,0);
+    label cellI=0;
+
+    vector Us(0,0,0);
+    vector Ur(0,0,0);
+    scalar ds(0);
+    scalar nuf(0);
+    scalar rho(0);
+    scalar magUr(0);
+    scalar Rep(0);
+    scalar localPhiP(0);
+
+    vector dragExplicit(0,0,0);
+    scalar dragCoefficient(0);
+    
+    interpolationCellPoint<scalar> voidfractionInterpolator_(voidfraction_);
+    interpolationCellPoint<vector> UInterpolator_(U_);
+
+    #include "setupProbeModel.H"
+
+    for(int index = 0;index <  particleCloud_.numberOfParticles(); ++index)
+    {
+        //if(mask[index][0])
+        //{
+            cellI = particleCloud_.cellIDs()[index][0];
+            drag = vector(0,0,0);
+            dragExplicit = vector(0,0,0);
+            Ufluid =vector(0,0,0);
+            voidfraction=0;
+            dragCoefficient = 0;
+
+            if (cellI > -1) // particle Found
+            {
+
+                if( forceSubM(0).interpolation() )
+                {
+                    position     = particleCloud_.position(index);
+                    voidfraction = voidfractionInterpolator_.interpolate(position,cellI);
+                    Ufluid       = UInterpolator_.interpolate(position,cellI);
+                    //Ensure interpolated void fraction to be meaningful
+                    // Info << " --> voidfraction: " << voidfraction << endl;
+                    if(voidfraction>1.00) voidfraction = 1.0;
+                    if(voidfraction<0.10) voidfraction = 0.10;
+                }
+                else
+                {
+                    voidfraction = voidfraction_[cellI];
+                    Ufluid = U_[cellI];
+                }
+
+                Us = particleCloud_.velocity(index);
+                Ur = Ufluid-Us;
+                magUr = mag(Ur);
+                ds = 2*particleCloud_.radius(index);
+                rho = rhoField[cellI];
+                nuf = nufField[cellI];
+
+                Rep=0.0;
+                localPhiP = 1.0f-voidfraction+SMALL;
+
+                // calc particle's drag coefficient (i.e., Force per unit slip velocity and per m³ PARTICLE)
+               
+                Rep=ds/scaleDia_*voidfraction*magUr/nuf+SMALL;
+                dragCoefficient = 10.0*localPhiP/(voidfraction*voidfraction) +
+                                  voidfraction*voidfraction*(1.0+1.5*Foam::sqrt(localPhiP)) +
+                                  0.413*Rep/(24*voidfraction*voidfraction)*(1.0/voidfraction+3*voidfraction*localPhiP+8.4*Foam::pow(Rep,-0.343))/
+                                  (1+Foam::pow(10,3*localPhiP)*Foam::pow(Rep,0.5*(1+4*localPhiP)));
+
+                // calc particle's drag
+                dragCoefficient *= 3*M_PI*ds*nuf*rho*voidfraction*scaleDrag_;
+                if (modelType_=="B")
+                    dragCoefficient /= voidfraction;
+
+                drag = dragCoefficient * Ur;
+
+                // explicitCorr
+                forceSubM(0).explicitCorr(drag,dragExplicit,dragCoefficient,Ufluid,U_[cellI],Us,UsField_[cellI],forceSubM(0).verbose());
+
+                if(forceSubM(0).verbose() && index >=0 && index <2)
+                {
+                    Pout << "cellI = " << cellI << endl;
+                    Pout << "index = " << index << endl;
+                    Pout << "Us = " << Us << endl;
+                    Pout << "Ur = " << Ur << endl;
+                    Pout << "ds = " << ds << endl;
+                    Pout << "ds/scale = " << ds/scaleDia_ << endl;
+                    Pout << "rho = " << rho << endl;
+                    Pout << "nuf = " << nuf << endl;
+                    Pout << "voidfraction = " << voidfraction << endl;
+                    Pout << "Rep = " << Rep << endl;
+                    Pout << "drag = " << drag << endl;
+                }
+
+                //Set value fields and write the probe
+                if(probeIt_)
+                {
+                    #include "setupProbeModelfields.H"
+                    vValues.append(drag);   //first entry must the be the force
+                    vValues.append(Ur);
+                    sValues.append(Rep);
+                    sValues.append(voidfraction);
+                    particleCloud_.probeM().writeProbe(index, sValues, vValues);
+                }
+            }
+
+            // write particle based data to global array
+            forceSubM(0).partToArray(index,drag,dragExplicit,Ufluid,dragCoefficient);
+
+        //}// end if mask
+    }// end loop particles
+}
+
+
+// * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
+
+} // End namespace Foam
+
+// ************************************************************************* //
