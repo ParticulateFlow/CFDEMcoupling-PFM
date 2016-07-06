29ec96de524b5bba766ac284032e05d9b05d9619
Thomas Lichtenegger <thomas.lichtenegger@jku.at>
2016-07-06 17:20:24 +0200

    New forceSubModel to scale force with user-defined field.

---
+++ src/lagrangian/cfdemParticle/subModels/forceModel/forceSubModels/ScaleForce/ScaleForce.C
@@ -0,0 +1,96 @@
+/*---------------------------------------------------------------------------*\
+    CFDEMcoupling - Open Source CFD-DEM coupling
+
+    CFDEMcoupling is part of the CFDEMproject
+    www.cfdem.com
+                                Christoph Goniva, christoph.goniva@cfdem.com
+                                Copyright 2009-2012 JKU Linz
+                                Copyright 2012-     DCS Computing GmbH, Linz
+-------------------------------------------------------------------------------
+License
+    This file is part of CFDEMcoupling.
+
+    CFDEMcoupling is free software; you can redistribute it and/or modify it
+    under the terms of the GNU General Public License as published by the
+    Free Software Foundation; either version 3 of the License, or (at your
+    option) any later version.
+
+    CFDEMcoupling is distributed in the hope that it will be useful, but WITHOUT
+    ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+    FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+    for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with CFDEMcoupling; if not, write to the Free Software Foundation,
+    Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA
+
+Description
+    This code is designed to realize coupled CFD-DEM simulations using LIGGGHTS
+    and OpenFOAM(R). Note: this code is not part of OpenFOAM(R) (see DISCLAIMER).
+\*---------------------------------------------------------------------------*/
+
+#include "error.H"
+
+#include "ScaleForce.H"
+#include "addToRunTimeSelectionTable.H"
+
+// * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
+
+namespace Foam
+{
+
+// * * * * * * * * * * * * * * Static Data Members * * * * * * * * * * * * * //
+
+defineTypeNameAndDebug(ScaleForce, 0);
+
+addToRunTimeSelectionTable
+(
+    forceSubModel,
+    ScaleForce,
+    dictionary
+);
+
+
+// * * * * * * * * * * * * * * * * Constructors  * * * * * * * * * * * * * * //
+
+// Construct from components
+ScaleForce::ScaleForce
+(
+    const dictionary& dict,
+    cfdemCloud& sm,
+    forceModel& fm
+)
+:
+    forceSubModel(dict,sm,fm),
+    propsDict_(dict.subDict(typeName + "Props")),
+    scaleFieldName_(propsDict_.lookup("scaleFieldName")),
+    scaleField_(sm.mesh().lookupObject<volScalarField> (scaleFieldName_))
+{}
+
+
+// * * * * * * * * * * * * * * * * Destructor  * * * * * * * * * * * * * * * //
+
+ScaleForce::~ScaleForce()
+{}
+
+
+// * * * * * * * * * * * * * * * Member Functions  * * * * * * * * * * * * * //
+void ScaleForce::partToArray
+(
+    label& index,
+    vector& dragTot,
+    const vector& dragEx,
+    const vector& Ufluid,
+    scalar Cd
+) const
+{
+    label cellI = particleCloud_.particleCell(index);
+    dragTot *= scaleField_[cellI];
+    forceSubModel::partToArray(index,dragTot,dragEx,Ufluid,Cd);
+}
+
+// * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
+
+} // End namespace Foam
+
+// ************************************************************************* //
