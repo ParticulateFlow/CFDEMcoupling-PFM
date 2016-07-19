ea88de98085e7b3eb48961c1842c29c030d1a2da
ekinaci <efekinaci@yahoo.com>
2016-07-19 14:35:16 +0200

    seperated pisoTemp solver & changed cloud energy

--- src/lagrangian/cfdemParticle/derived/cfdemCloudEnergy/cfdemCloudEnergy.C
+++ src/lagrangian/cfdemParticle/derived/cfdemCloudEnergy/cfdemCloudEnergy.C
@@ -150,12 +150,12 @@ bool cfdemCloudEnergy::evolve
         if(verbose_) Info << "calcEnergyContributions done." << endl;
         clockM().stop("calcEnergyContributions");
 
-        // execute chemical model species
+        /*// execute chemical model species
         clockM().start(28,"speciesExecute");
         if(verbose_) Info << "- speciesExecute()" << endl;
         speciesExecute();
         if(verbose_) Info << "speciesExecute done" << endl;
-        clockM().stop("speciesExecute");
+        clockM().stop("speciesExecute");*/
 
 	return true;
     }
