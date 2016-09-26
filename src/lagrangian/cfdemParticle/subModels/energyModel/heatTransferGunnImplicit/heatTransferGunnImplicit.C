e268853de88593ded39d3ac28d0372187426f19e
Thomas Lichtenegger <thomas.lichtenegger@jku.at>
2016-09-26 13:28:31 +0200

    Fixing minor errors.

--- src/lagrangian/cfdemParticle/subModels/energyModel/heatTransferGunnImplicit/heatTransferGunnImplicit.C
+++ src/lagrangian/cfdemParticle/subModels/energyModel/heatTransferGunnImplicit/heatTransferGunnImplicit.C
@@ -74,7 +74,7 @@ heatTransferGunnImplicit::~heatTransferGunnImplicit()
 // * * * * * * * * * * * * * * * private Member Functions  * * * * * * * * * * * * * //
 void heatTransferGunnImplicit::allocateMyArrays() const
 {
-    heatTransferGunn::allocateMyArrays();
+  //  heatTransferGunn::allocateMyArrays();
     double initVal=0.0;
     particleCloud_.dataExchangeM().allocateArray(partHeatFluxCoeff_,initVal,1);
 }
@@ -82,7 +82,11 @@ void heatTransferGunnImplicit::allocateMyArrays() const
 
 void heatTransferGunnImplicit::calcEnergyContribution()
 {
+    allocateMyArrays();
+
     heatTransferGunn::calcEnergyContribution();
+
+    QPartFluidCoeff_.primitiveFieldRef() = 0.0;
     
     particleCloud_.averagingM().setScalarSum
     (
@@ -94,8 +98,7 @@ void heatTransferGunnImplicit::calcEnergyContribution()
 
     QPartFluidCoeff_.primitiveFieldRef() /= -QPartFluidCoeff_.mesh().V();
     
-    QPartFluidCoeff_.correctBoundaryConditions();
-
+  //  QPartFluidCoeff_.correctBoundaryConditions();
 }
 
 void heatTransferGunnImplicit::addEnergyCoefficient(volScalarField& Qsource) const
