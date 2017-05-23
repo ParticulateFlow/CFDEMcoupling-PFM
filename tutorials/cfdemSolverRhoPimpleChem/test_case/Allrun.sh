04119c6934877fa4214dcd2527b53a0001a5114c
ekinaci <efekinaci@yahoo.com>
2017-05-23 09:49:22 +0200

    update tutorial case

--- tutorials/cfdemSolverRhoPimpleChem/test_case/Allrun.sh
+++ tutorials/cfdemSolverRhoPimpleChem/test_case/Allrun.sh
@@ -6,10 +6,20 @@
 # Christoph Goniva - Sept. 2010
 #===================================================================#
 
+#- source CFDEM env vars
+. ~/.bashrc
+
+#- include functions
+source $CFDEM_SRC_DIR/lagrangian/cfdemParticle/etc/functions.sh
+
 #- define variables
 casePath="$(dirname "$(readlink -f ${BASH_SOURCE[0]})")"
-export casePath
+headerText="test_case"
+logfileName="$casePath/log_$headerText"
+solverName="cfdemSolverRhoPimpleChem"
+nrProcs="4"
 
+export casePath
 # check if mesh was built
 if [ -f "$casePath/CFD/constant/polyMesh/points" ]; then
     echo "mesh was built before - using old mesh"
@@ -26,6 +36,11 @@ else
     $casePath/parDEMrun.sh
 fi
 
+#echo "Run Simulation"
+#cd $casePath/CFD
+#decomposePar    
+#mpirun -np $nrProcs $solverName -parallel | tee -a $logfileName 
+
 #- run parallel CFD-DEM in new terminal
-gnome-terminal -e "bash $casePath/parCFDDEMrun.sh" 
-#bash $casePath/parCFDDEMrun.sh
+#gnome-terminal -e "bash $casePath/parCFDDEMrun.sh"
+bash $casePath/parCFDDEMrun.sh 
--- tutorials/cfdemSolverRhoPimpleChem/test_case/postproc.sh
+++ tutorials/cfdemSolverRhoPimpleChem/test_case/postproc.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-grep "dmB\[0]" log_3layerUnreactedShrinkingCore > dmB0.dat
-grep "dmB\[1]" log_3layerUnreactedShrinkingCore > dmB1.dat
-grep "dmB\[2]" log_3layerUnreactedShrinkingCore > dmB2.dat
-grep "dmB\[3]" log_3layerUnreactedShrinkingCore > dmB3.dat
+grep "dmB\[0]" log_test_case > dmB0.dat
+grep "dmB\[1]" log_test_case > dmB1.dat
+grep "dmB\[2]" log_test_case > dmB2.dat
+grep "dmB\[3]" log_test_case > dmB3.dat
 #grep -n "pre-layerMass[0]:" log_3layerUnreactedShrinkingCore > pre_particle_mass0.dat
 #grep -n "pre-layerMass[1]:" log_3layerUnreactedShrinkingCore > pre_particle_mass1.dat
 #grep -n "pre-layerMass[2]:" log_3layerUnreactedShrinkingCore > pre_particle_mass2.dat
