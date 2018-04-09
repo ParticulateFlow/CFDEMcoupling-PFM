2b07600de221e397c0522e8d05c34a5037f296d1
asanaz <sanaz.abbassi@gmail.com>
2018-04-09 12:50:46 +0200

    modified mapField script for dataBase

--- applications/utilities/mapDataBase/mapFieldData.sh
+++ applications/utilities/mapDataBase/mapFieldData.sh
@@ -1,45 +1,30 @@
-#in controlDict startFrom is "latestTime".
-#The first loop is just needed for time directories smaller than 1.
+#!/bin/sh
+# Source run functions
+. $WM_PROJECT_DIR/bin/tools/RunFunctions
 
-for number in {0..100..1}
+# to be executed from one level above the source directory
+
+if [ $# -eq 0 ]
+  then
+    sourceName="dataBase"
+    targetName="dataBaseCoarse"
+  else
+    sourceName=$1
+    targetName=$2
+fi
+
+cd $sourceName
+
+for time in *
 do
-
- if (($number % 100 == 0))
- then
- da=$(bc <<< "($number/100)")
- mkdir "$da"
- mapFields /path/to/source -sourceTime $da -consistent 
- elif (($number % 10 == 0))
- then
- da=$(bc <<< "scale=1;($number/100)")
- mkdir "0$da"
- mapFields /path/to/source -sourceTime $da -consistent
- else
- da=$(bc <<< "scale=2;($number/100)")
- mkdir "0$da"
- mapFields /path/to/source -sourceTime $da -consistent 
-
- fi
-done
-
-for number in {101..2500..1}
-do
-
- if (($number % 100 == 0))
- then
- da=$(bc <<< "($number/100)")
- mkdir "$da"
- mapFields /path/to/source -sourceTime $da -consistent 
- elif (($number % 10 == 0))
- then
- da=$(bc <<< "scale=1;($number/100)")
- mkdir "$da"
- mapFields /path/to/source -sourceTime $da -consistent
- else
- da=$(bc <<< "scale=2;($number/100)")
- mkdir "$da"
- mapFields /path/to/source -sourceTime $da -consistent 
-
- fi
+    if [ $time != "system" ] && [ $time != "constant" ];
+    then
+        cd ../$targetName
+        echo "Found $time."
+        sed -i "/^startTime/c\startTime \t$time;" ./system/controlDict
+        grep 'startTime' ./system/controlDict
+        mapFields ../$sourceName -sourceTime $time -consistent
+        cd ../$sourceName
+    fi
 done
 
