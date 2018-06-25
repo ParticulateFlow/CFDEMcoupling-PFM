54b88ed8732fa9cb2309f0193d5b142121eb25e2
s126103 <t.j.m.nijssen@tue.nl>
2018-06-25 10:04:07 +0200

    Loosened timeStepFraction constraint, preventing unnecessary crashes

--- src/lagrangian/cfdemParticle/cfdemCloud/cfdemCloud.C
+++ src/lagrangian/cfdemParticle/cfdemCloud/cfdemCloud.C
@@ -629,7 +629,7 @@ bool cfdemCloud::evolve
         //      IMPLICIT FORCE CONTRIBUTION AND SOLVER USE EXACTLY THE SAME AVERAGED
         //      QUANTITIES AT THE GRID!
         Info << "\n timeStepFraction() = " << dataExchangeM().timeStepFraction() << endl;
-        if(dataExchangeM().timeStepFraction() > 1.0000001)
+        if(dataExchangeM().timeStepFraction() > 1.001)
         {
             FatalError << "cfdemCloud::dataExchangeM().timeStepFraction()>1: Do not do this, since dangerous. This might be due to the fact that you used a adjustable CFD time step. Please use a fixed CFD time step." << abort(FatalError);
         }
