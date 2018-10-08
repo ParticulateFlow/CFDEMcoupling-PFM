ParticulateFlow/CFDEMcoupling-PFM

commit 2230f514214a3ad5c8b78b7493d9ebbc4e12b15e

changed back the handling of verbose flag

src/lagrangian/cfdemParticle/cfdemCloud/cfdemCloud.C
@@ -78,11 +78,11 @@ cfdemCloud::cfdemCloud
             IOobject::NO_WRITE
         )
     ),
-    solveFlow_(couplingProperties_.lookupOrDefault<bool>("solveFlow", true)),
-    verbose_(couplingProperties_.lookupOrDefault<bool>("verbose", false)),
-    ignore_(couplingProperties_.lookupOrDefault<bool>("ignore", false)),
+    solveFlow_(couplingProperties_.found("solveFlow")),
+    verbose_(couplingProperties_.found("verbose")),
+    ignore_(couplingProperties_.found("ignore")),
     allowCFDsubTimestep_(true),
-    limitDEMForces_(couplingProperties_.lookupOrDefault<bool>("limitDEMForces",false)),
+    limitDEMForces_(couplingProperties_.found("limitDEMForces")),
     maxDEMForce_(0.),
     modelType_(couplingProperties_.lookup("modelType")),
     positions_(NULL),
@@ -224,8 +224,7 @@ cfdemCloud::cfdemCloud
     buildInfo.info();
 
     Info << "If BC are important, please provide volScalarFields -imp/expParticleForces-" << endl;
-    /*if (couplingProperties_.found("solveFlow"))
-        solveFlow_=Switch(couplingProperties_.lookup("solveFlow"));*/
+
     if (couplingProperties_.found("imExSplitFactor"))
         imExSplitFactor_ = readScalar(couplingProperties_.lookup("imExSplitFactor"));
 

src/lagrangian/cfdemParticle/subModels/forceModel/KochHillRWDrag/KochHillRWDrag.C
@@ -64,16 +64,16 @@ KochHillRWDrag::KochHillRWDrag
 :
     forceModel(dict,sm),
     propsDict_(dict.subDict(typeName + "Props")),
-    verbose_(propsDict_.lookupOrDefault<bool>("verbose", false)),
+    verbose_(propsDict_.found("verbose")),
     velFieldName_(propsDict_.lookup("velFieldName")),
     U_(sm.mesh().lookupObject<volVectorField> (velFieldName_)),
     voidfractionFieldName_(propsDict_.lookup("voidfractionFieldName")),
     voidfraction_(sm.mesh().lookupObject<volScalarField> (voidfractionFieldName_)),
     UsFieldName_(propsDict_.lookupOrDefault("granVelFieldName",word("Us"))),
     UsField_(sm.mesh().lookupObject<volVectorField> (UsFieldName_)),
-    interpolation_(propsDict_.lookupOrDefault<bool>("interpolation", false)),
+    interpolation_(propsDict_.found("interpolation")),
     scale_(1.),
-    randomTauE_(propsDict_.lookupOrDefault<bool>("randomTauE", false)),
+    randomTauE_(propsDict_.found("randomTauE")),
     partTime_(NULL),
     partUfluct_(NULL),
     RanGen_(label(0))

src/lagrangian/cfdemParticle/subModels/forceModel/SchillerNaumannDrag/SchillerNaumannDrag.C
@@ -64,7 +64,7 @@ SchillerNaumannDrag::SchillerNaumannDrag
 :
     forceModel(dict,sm),
     propsDict_(dict.subDict(typeName + "Props")),
-    verbose_(propsDict_.lookupOrDefault<bool>("verbose", false)),
+    verbose_(propsDict_.found("verbose")),
     velFieldName_(propsDict_.lookup("velFieldName")),
     U_(sm.mesh().lookupObject<volVectorField> (velFieldName_))
 {

src/lagrangian/cfdemParticle/subModels/forceModel/ShirgaonkarIB/ShirgaonkarIB.C
@@ -63,8 +63,8 @@ ShirgaonkarIB::ShirgaonkarIB
 :
     forceModel(dict,sm),
     propsDict_(dict.subDict(typeName + "Props")),
-    verbose_(propsDict_.lookupOrDefault<bool>("verbose", false)),
-    twoDimensional_(propsDict_.lookupOrDefault<bool>("twoDimensional", false)),
+    verbose_(propsDict_.found("verbose")),
+    twoDimensional_(propsDict_.found("twoDimensional")),
     depth_(1),
     velFieldName_(propsDict_.lookup("velFieldName")),
     U_(sm.mesh().lookupObject<volVectorField> (velFieldName_)),

src/lagrangian/cfdemParticle/subModels/forceModel/noDrag/noDrag.C
@@ -62,9 +62,9 @@ noDrag::noDrag
 :
     forceModel(dict,sm),
     propsDict_(dict),
-    verbose_(propsDict_.lookupOrDefault<bool>("verbose", false)),
-    noDEMForce_(propsDict_.lookupOrDefault<bool>("noDEMForce", false)),
-    keepCFDForce_(propsDict_.lookupOrDefault<bool>("keepCFDForce", false))
+    verbose_(propsDict_.found("verbose")),
+    noDEMForce_(propsDict_.found("noDEMForce")),
+    keepCFDForce_(propsDict_.found("keepCFDForce"))
 {
     if(dict.found(word(typeName + "Props")))
         propsDict_=dictionary(dict.subDict(typeName + "Props"));

src/lagrangian/cfdemParticle/subModels/forceModel/particleCellVolume/particleCellVolume.C
@@ -94,7 +94,7 @@ particleCellVolume::particleCellVolume
     ),
     upperThreshold_(readScalar(propsDict_.lookup("upperThreshold"))),
     lowerThreshold_(readScalar(propsDict_.lookup("lowerThreshold"))),
-    verbose_(propsDict_.lookupOrDefault<bool>("verbose", false))
+    verbose_(propsDict_.found("verbose"))
 {
 }
 

src/lagrangian/cfdemParticle/subModels/forceModel/virtualMassForce/virtualMassForce.C
@@ -70,7 +70,7 @@ virtualMassForce::virtualMassForce
     phiFieldName_(propsDict_.lookup("phiFieldName")),
     phi_(sm.mesh().lookupObject<surfaceScalarField> (phiFieldName_)),
     UrelOld_(NULL),
-    splitUrelCalculation_(propsDict_.lookupOrDefault<bool>("splitUrelCalculation", false)),
+    splitUrelCalculation_(propsDict_.found("splitUrelCalculation")),
     Cadd_(0.5)
 {
 

src/lagrangian/cfdemParticle/subModels/forceModel/volWeightedAverage/volWeightedAverage.C
@@ -69,7 +69,7 @@ volWeightedAverage::volWeightedAverage
     vectorFieldNames_(propsDict_.lookup("vectorFieldNames")),
     upperThreshold_(readScalar(propsDict_.lookup("upperThreshold"))),
     lowerThreshold_(readScalar(propsDict_.lookup("lowerThreshold"))),
-    verbose_(propsDict_.lookupOrDefault<bool>("verbose", false))
+    verbose_(propsDict_.found("verbose"))
 {
     // create vol weighted average scalar fields
     scalarFields_.setSize(scalarFieldNames_.size());

commit 3e1c09930bf7be27d1db9d010a0b044c7f885760

fix bug introduced in commit 2230f514214a3ad5c8b78b7493d9ebbc4e12b15e
wrong default value

src/lagrangian/cfdemParticle/cfdemCloud/cfdemCloud.C
@@ -78,7 +78,7 @@ cfdemCloud::cfdemCloud
             IOobject::NO_WRITE
         )
     ),
-    solveFlow_(couplingProperties_.found("solveFlow")),
+    solveFlow_(couplingProperties_.lookupOrDefault<bool>("solveFlow", true)),
     verbose_(couplingProperties_.found("verbose")),
     ignore_(couplingProperties_.found("ignore")),
     allowCFDsubTimestep_(true),