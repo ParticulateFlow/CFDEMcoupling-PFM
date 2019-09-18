7fb0161ca89db2b8f5ec2d56d07f0542f78104f5
ekinaci <efekinaci@yahoo.com>
2019-09-18 17:51:18 +0200

    Add tutorial case for polydisperse R2 reaction with inert Quartz particles

---
+++ tutorials/cfdemSolverRhoPimpleChem/PolydisperseFluidizedBed/R2_FB/Allrun.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#- source CFDEM env vars
+. ~/.bashrc
+
+#- include functions
+source $CFDEM_PROJECT_DIR/etc/functions.sh
+
+#- define variables
+casePath="$(dirname "$(readlink -f ${BASH_SOURCE[0]})")"
+export casePath
+cd $casePath/CFD/
+blockMesh
+
+echo "Run Simulation"
+bash $casePath/parCFDDEMrun.sh   
