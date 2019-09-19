4ac94e82c7e9475b424408eba22006fa2d236477
Daniel Queteschiner <daniel.queteschiner@jku.at>
2019-09-19 16:40:07 +0200

    fix up Allclean.sh scripts [ci skip]
    note that #!/bin/sh may invoke a different shell than #!/bin/bash

--- tutorials/cfdemSolverRhoPimpleChem/PolydisperseFluidizedBed/R2_FB/Allclean.sh
+++ tutorials/cfdemSolverRhoPimpleChem/PolydisperseFluidizedBed/R2_FB/Allclean.sh
@@ -1,21 +1,15 @@
 #!/bin/sh
-#cd ${0%/*} || exit 1    # run from this directory
+cd ${0%/*} || exit 1    # run from this directory
 
 # Source tutorial run functions
 . $WM_PROJECT_DIR/bin/tools/CleanFunctions
-source $CFDEM_PROJECT_DIR/etc/functions.sh
 
-#- define variables
-casePath="$(dirname "$(readlink -f ${BASH_SOURCE[0]})")"
-cd $casePath/CFD
+cd CFD
 cleanCase
-rm $casePath/log*
+cd - 
 
-rm $casePath/DEM/post/liggghts_run*
-rm $casePath/DEM/post/dump.liggghts_run
-#mkdir $casePath/DEM/post
-#mkdir $casePath/DEM/post/restart
-#cd $casePath/DEM/post/restart 
-#touch liggghts.restart
+rm -f log*
+
+rm ./DEM/post/dump*
 
 # ----------------------------------------------------------------- end-of-file
