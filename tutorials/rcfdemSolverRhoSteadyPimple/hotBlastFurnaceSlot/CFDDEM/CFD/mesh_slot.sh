332dc5e057de28f71090453af6526ad06bbd627f
tlichtenegger <thomas.lichtenegger@jku.at>
2020-02-10 07:41:33 +0100

    New tutorial case for BF with dynamic cohesive zone.

---
+++ tutorials/rcfdemSolverRhoSteadyPimple/hotBlastFurnaceSlot/CFDDEM/CFD/mesh_slot.sh
@@ -0,0 +1,17 @@
+./Allclean
+cd constant/polyMesh
+m4 -P blockMeshDict_slot.m4 > blockMeshDict
+cd ../..
+blockMesh
+mirrorMesh
+
+topoSet -dict system/topoSetDict
+createPatch -overwrite
+rm -r 0
+cp -r orig.0/ 0
+
+setsToZones -noFlipMap
+
+#foamToSurface surface.stl
+#surfaceSplitByPatch surface.stl
+#mv surface_*.stl stl_files/
