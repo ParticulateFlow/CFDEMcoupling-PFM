d47e4d01a793270f389aacc4b7d945f80f504f5b
tlichtenegger <thomas.lichtenegger@jku.at>
2022-03-08 16:28:14 +0100

    Changed way to read in large data file.

--- src/lagrangian/cfdemParticle/subModels/chemistryModel/initMultiLayers/initMultiLayers.C
+++ src/lagrangian/cfdemParticle/subModels/chemistryModel/initMultiLayers/initMultiLayers.C
@@ -57,6 +57,7 @@ initMultiLayers::initMultiLayers
     mesh_(sm.mesh()),
     verbose_(propsDict_.lookupOrDefault<bool>("verbose",false)),
     maxNumLayers_(0),
+    maxNumParticlesPerType_(propsDict_.lookupOrDefault<label>("maxNumParticlesPerType",1000000)),
     numLayers_(propsDict_.lookupOrDefault<labelList>("numLayers",labelList(1,-1))),
     partTypes_(propsDict_.lookupOrDefault<labelList>("partTypes",labelList(1,-1))),
     searchLayers_(propsDict_.lookupOrDefault<labelList>("searchLayers",labelList(1,-1))),
@@ -108,7 +109,7 @@ bool initMultiLayers::init()
         }
         Info << "\nReading" << endl;
         Info << "\t" << filename << endl;
-        readDump(filename, type, indices, positions, relradii);
+        label numLines = readDump(filename, type, indices, positions, relradii);
 
         Info << "Binning particle displacements on mesh for type " << type << endl;
 
@@ -145,7 +146,7 @@ bool initMultiLayers::init()
         );
 
         label cellI = -1;
-        for (label lineI = 0; lineI < indices.size(); lineI++)
+        for (label lineI = 0; lineI < numLines; lineI++)
         {
             position = positions[lineI];
             relrad = relradii[lineI];
@@ -269,7 +270,7 @@ label initMultiLayers::getListIndex(label testElement) const
     return -1;
 }
 
-void initMultiLayers::readDump(std::string filename, label type, labelList &indices, vectorList &positions, vectorList &relradii)
+label initMultiLayers::readDump(std::string filename, label type, labelList &indices, vectorList &positions, vectorList &relradii)
 {
     #include <fstream>
 
@@ -286,6 +287,10 @@ void initMultiLayers::readDump(std::string filename, label type, labelList &indi
     positions.clear();
     relradii.clear();
 
+    indices.setSize(maxNumParticlesPerType_);
+    positions.setSize(maxNumParticlesPerType_);
+    relradii.setSize(maxNumParticlesPerType_);
+
     std::ifstream file(filename);
     std::string str;
     while (std::getline(file, str))
@@ -297,12 +302,17 @@ void initMultiLayers::readDump(std::string filename, label type, labelList &indi
             {
                 FatalError<< "Particle of type " << partType << " detected in " << filename << abort(FatalError);
             }
-            indices.append(partIndex);
-            positions.append(vector(x,y,z));
-            relradii.append(vector(r1,r2,r3));
+
+            indices[lineCounter-leadingLines] = partIndex;
+            positions[lineCounter-leadingLines] = vector(x,y,z);
+            relradii[lineCounter-leadingLines] = vector(r1,r2,r3);
         }
         lineCounter++;
+        if (lineCounter == maxNumParticlesPerType_) break;
     }
+
+    label readLines = lineCounter - leadingLines;
+    return readLines;
 }
 // * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
 
