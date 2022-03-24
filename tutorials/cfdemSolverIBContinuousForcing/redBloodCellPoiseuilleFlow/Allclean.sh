cc19fd3a467bc53714efaa41b62759c0e7b362aa
danielque <daniel.queteschiner@jku.at>
2022-03-24 11:06:35 +0100

    clean up Allclean.sh of redBloodCellPoiseuilleFlow case

--- tutorials/cfdemSolverIBContinuousForcing/redBloodCellPoiseuilleFlow/Allclean.sh
+++ tutorials/cfdemSolverIBContinuousForcing/redBloodCellPoiseuilleFlow/Allclean.sh
@@ -11,34 +11,21 @@
 #- source CFDEM functions
 source $CFDEM_PROJECT_DIR/etc/functions.sh
 
-#--------------------------------------------------------------------------------#
+#------------------------------------------------------------------------------
 #- define variables
 casePath="$(dirname "$(readlink -f ${BASH_SOURCE[0]})")"
-logpath=$casePath
-headerText="run_parallel_cfdemSolverIBContinuousForcing_redBloodCellPoiseuilleFlow"
-logfileName="log_$headerText"
-solverName="cfdemSolverIBContinuousForcing"
-nrProcs="2"
-machineFileName="none"   # yourMachinefileName | none
-debugMode="off"          # on | off| strict
-testHarnessPath="$CFDEM_TEST_HARNESS_PATH"
-runOctave="false"
-postproc="true"
-#--------------------------------------------------------------------------------#
 
-#- copy log file to test harness
-cp ../../$logfileName $testHarnessPath
-
+#------------------------------------------------------------------------------
 #- clean up case
 echo "deleting data at: $casePath"
 source $WM_PROJECT_DIR/bin/tools/CleanFunctions
 cd $casePath/CFD
 cleanTimeDirectories
-rm -rf processor*
-rm -r $casePath/CFD/clockData
-rm $casePath/DEM/post/*.*
+rm -rf processor* > /dev/null 2>&1
+rm -rf clockData > /dev/null 2>&1
+echo "Cleaning $casePath/DEM/post"
+rm $casePath/DEM/post/*.*  > /dev/null 2>&1
 #rm -r $casePath/DEM/post/restart/*.*
-#- preserve post directory
-touch $casePath/DEM/post/.gitignore
+#touch $casePath/DEM/post/restart/.gitignore
 echo "done"
 
