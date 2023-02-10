4e65f19d5fbef0b7d67d6a1062e759fef264ccdd
danielque <daniel.queteschiner@jku.at>
2023-02-10 11:12:17 +0100

    add FeO->Fe reduction case
    based on R1 case described by Spreitzer 2016 / Kinaci 2020

---
+++ tutorials/cfdemSolverRhoPimpleChem/PolydisperseFluidizedBed/R1_FB/Allrun.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+#------------------------------------------------------------------------------
+# Allrun script for fluidized bed R1 chemistry test case
+# run R1_FB test case
+# Daniel Queteschiner - March 2022
+#------------------------------------------------------------------------------
+
+#- source CFDEM env vars
+. ~/.bashrc
+
+#- include functions
+source $CFDEM_PROJECT_DIR/etc/functions.sh
+
+#- define variables
+casePath="$(dirname "$(readlink -f ${BASH_SOURCE[0]})")"
+
+#- check if mesh was built
+if [ -f "$casePath/CFD/constant/polyMesh/points" ]; then
+    echo "mesh was built before - using old mesh"
+else
+    echo "mesh needs to be built"
+    cd $casePath/CFD
+    blockMesh
+fi
+
+if [ -f "$casePath/DEM/post/restart/liggghts.restart" ];  then
+    echo "LIGGGHTS init was run before - using existing restart file"
+else
+    #- run parallel DEM
+    $casePath/parDEMrun.sh
+fi
+
+#- run parallel CFD-DEM
+bash $casePath/parCFDDEMrun.sh
---
+++ tutorials/cfdemSolverRhoPimpleChem/PolydisperseFluidizedBed/R1_FB/CFD/octave/plotData.m
@@ -0,0 +1,70 @@
+#!/usr/bin/octave --silent
+
+%% clear workspace
+clear all
+close all
+clc
+
+dirfile = '../../DEM/post';
+filepattern = '*.dat';
+
+% time column in the data matrix
+col_t = 1;
+
+% read all data
+
+listfile = dir(fullfile(dirfile,filepattern));
+data = readData(dirfile,listfile);
+nFiles = length(listfile);
+dataexp = importdata('R1_experiment.dat', ' ');
+
+% init figures
+hFig(1) = figure;
+
+cmap = colormap(jet(16));
+linS = {'-';'--';'-.';':';'-';'--';'-.';':';'-';'--';'-.';':'};
+markers = {'+';'o';'*';'x';'s';'d';'^';'v';'>';'<';'p';'h';'.'};
+
+for ii=1:nFiles
+
+    fname = data(ii).name;
+    fbasename = strtrunc(fname,index(fname,".dat")-1);
+    stepsize = 2;
+    timesteps = data(ii).values(1:stepsize:end,col_t)*10;
+    nColumns = columns(data(ii).values);
+
+    figure(hFig(1));
+    clf reset;
+    hold on;
+
+    for jj=2:nColumns
+        xvalue = data(ii).values(1:stepsize:end,jj);
+        %if (strncmp(fbasename,"Aterm",5) || strncmp(fbasename,"Bterm",5))
+        %    semilogy(timesteps,xvalue,'Color',cmap(jj,:),'LineStyle',linS{jj-1},'Marker',markers{jj-1},'MarkerSize',5);
+        %else
+            plot(timesteps,xvalue,'Color',cmap(jj,:),'LineStyle',linS{jj-1},'Marker',markers{jj-1},'MarkerSize',5);
+        %endif
+    end
+    if (strncmp(fbasename,"fr_OV",5))
+        timesteps = dataexp.data(1:stepsize:end,col_t);
+        xvalue = dataexp.data(1:stepsize:end,2)*0.01;
+        plot(timesteps-450,xvalue,'Color','red','LineStyle','none','Marker','.','MarkerSize',10);%,'MarkerFaceColor','auto');
+    endif
+    xlim ([0.0, 2000.0]);
+    ylim auto;
+    xlabel('time (s)');
+    ylabel(fbasename);
+    grid on;
+
+    headerline = substr(data(ii).header{1},7);
+
+    if (strncmp(fbasename,"fr_OV",5))
+        headerline = [headerline, " fr_exp_OV"];
+    endif
+
+    legend(strsplit(headerline, " "),'location','eastoutside');
+
+    print(hFig(1),fullfile(dirfile,['figure_',fbasename,'.eps']),'-color','-deps');
+end
+
+
