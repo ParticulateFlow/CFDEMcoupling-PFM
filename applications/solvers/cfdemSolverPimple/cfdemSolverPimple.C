0629aa5883fad8e5b7b531c1567c19e3da08979c
danielque <daniel.queteschiner@jku.at>
2023-04-07 12:39:18 +0200

    fix up copyright information

--- applications/solvers/cfdemSolverPimple/cfdemSolverPimple.C
+++ applications/solvers/cfdemSolverPimple/cfdemSolverPimple.C
@@ -1,28 +1,22 @@
 /*---------------------------------------------------------------------------*\
-    CFDEMcoupling - Open Source CFD-DEM coupling
+    Open Source CFD-DEM coupling
 
-    CFDEMcoupling is part of the CFDEMproject
-    www.cfdem.com
-                                Christoph Goniva, christoph.goniva@cfdem.com
-                                Copyright (C) 1991-2009 OpenCFD Ltd.
-                                Copyright (C) 2009-2012 JKU, Linz
-                                Copyright (C) 2012-     DCS Computing GmbH,Linz
+    Copyright (C) 2023  Behrad Esgandari, JKU Linz, Austria
 -------------------------------------------------------------------------------
 License
-    This file is part of CFDEMcoupling.
 
-    CFDEMcoupling is free software: you can redistribute it and/or modify it
+    This program is free software: you can redistribute it and/or modify it
     under the terms of the GNU General Public License as published by
     the Free Software Foundation, either version 3 of the License, or
     (at your option) any later version.
 
-    CFDEMcoupling is distributed in the hope that it will be useful, but WITHOUT
+    This program is distributed in the hope that it will be useful, but WITHOUT
     ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
     FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
     for more details.
 
     You should have received a copy of the GNU General Public License
-    along with CFDEMcoupling.  If not, see <http://www.gnu.org/licenses/>.
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 Application
     cfdemSolverPimple
