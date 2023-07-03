b0e4905ffd895908c834ff36567f7ac159e0bd1c
danielque <daniel.queteschiner@jku.at>
2023-07-03 17:51:50 +0200

    clean up cfdemSolverRhoPimple/FinesColumnStickyMonodisperse tutorial

--- tutorials/cfdemSolverRhoPimple/FinesColumnStickyMonodisperse/parDEMrun.sh
+++ tutorials/cfdemSolverRhoPimple/FinesColumnStickyMonodisperse/parDEMrun.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
-
-#===================================================================#
+#------------------------------------------------------------------------------
 # DEM run script for FinesColumn testcase
 # init FinesColumn
 # Thomas Lichtenegger - June 2023
-#===================================================================#
+#------------------------------------------------------------------------------
 
 #- source CFDEM env vars
 . ~/.bashrc
@@ -13,7 +12,7 @@
 source $CFDEM_PROJECT_DIR/etc/functions.sh
 
 echo "starting DEM run in parallel..."
-#--------------------------------------------------------------------------------#
+#------------------------------------------------------------------------------
 #- define variables
 casePath="$(dirname "$(readlink -f ${BASH_SOURCE[0]})")"
 logpath="$casePath"
@@ -23,7 +22,7 @@ solverName="in.liggghts_init"
 nrProcs=8
 machineFileName="none"
 debugMode="off"
-#--------------------------------------------------------------------------------#
+#------------------------------------------------------------------------------
 
 #- call function to run DEM case
 parDEMrun $logpath $logfileName $casePath $headerText $solverName $nrProcs $machineFileName $debugMode
