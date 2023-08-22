d464002bc6301ffe23f8454554566ed4c6fa0d80
danielque <daniel.queteschiner@jku.at>
2023-08-22 13:05:35 +0200

    clean up whitespaces and fix headers in tutorial files

--- tutorials/cfdemSolverPimple/PeriodicBox/parDEMrun.sh
+++ tutorials/cfdemSolverPimple/PeriodicBox/parDEMrun.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
-
-#===================================================================#
+#------------------------------------------------------------------------------
 # DEM run script for periodic box testcase
 # init periodic box
 # Behrad Esgandari - August 2023
-#===================================================================#
+#------------------------------------------------------------------------------
 
 #- source CFDEM env vars
 . ~/.bashrc
@@ -13,7 +12,7 @@
 source $CFDEM_PROJECT_DIR/etc/functions.sh
 
 echo "starting DEM run in parallel..."
-#--------------------------------------------------------------------------------#
+#------------------------------------------------------------------------------
 #- define variables
 casePath="$(dirname "$(readlink -f ${BASH_SOURCE[0]})")"
 logpath="$casePath"
@@ -23,7 +22,7 @@ solverName="in.liggghts_init"
 nrProcs=32
 machineFileName="none"
 debugMode="off"
-#--------------------------------------------------------------------------------#
+#------------------------------------------------------------------------------
 
 #- call function to run DEM case
 parDEMrun $logpath $logfileName $casePath $headerText $solverName $nrProcs $machineFileName $debugMode
